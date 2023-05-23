56d38292b34e5e4aa63a8be6db89224afb87af69
Reyner Veliz <reyner@reynerveliz.com>
2023-05-23 11:07:06 -0600

    Save and restore image and string  from project resource

--- multi_lang_pro/res_test.Designer.vb
+++ multi_lang_pro/res_test.Designer.vb
@@ -22,7 +22,28 @@ Partial Class res_test
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(135, 95)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Load"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Location = New System.Drawing.Point(135, 171)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(250, 206)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 1
+        Me.PictureBox1.TabStop = False
         '
         'res_test
         '
@@ -30,9 +51,15 @@ Partial Class res_test
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackgroundImage = Global.multi_lang_pro.My.Resources.Resources.jet1
         Me.ClientSize = New System.Drawing.Size(559, 505)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.Button1)
         Me.Name = "res_test"
         Me.Text = "res_test"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
     End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents PictureBox1 As PictureBox
 End Class
--- multi_lang_pro/res_test.vb
+++ multi_lang_pro/res_test.vb
@@ -1,3 +1,5 @@
 ﻿Public Class res_test
-
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Me.PictureBox1.Image = My.Resources.jet1
+    End Sub
 End Class
