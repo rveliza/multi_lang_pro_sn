294920208a1079d1a7b90fd52ccfd4339af220cd
Reyner Veliz <reynerv@yahoo.com>
2023-06-05 21:32:51 -0500

    Change App Language by code

--- multi_lang_pro/My Project/Application.Designer.vb
+++ multi_lang_pro/My Project/Application.Designer.vb
@@ -29,10 +29,12 @@ Namespace My
             Me.SaveMySettingsOnExit = true
             Me.ShutDownStyle = Global.Microsoft.VisualBasic.ApplicationServices.ShutdownMode.AfterMainFormCloses
         End Sub
-        
-        <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
+
+        <Global.System.Diagnostics.DebuggerStepThroughAttribute()>
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.multi_lang_pro.res_test
+            Dim c As New System.Globalization.CultureInfo("ar")
+            System.Threading.Thread.CurrentThread.CurrentUICulture = c
+            Me.MainForm = Global.multi_lang_pro.Form1
         End Sub
     End Class
 End Namespace
