5c8c8004300b33117504e17cab49ee9997f380d8
Reyner Veliz <reynerv@yahoo.com>
2023-06-05 20:30:20 -0500

    Add arabic language

--- multi_lang_pro/Form1.vb
+++ multi_lang_pro/Form1.vb
@@ -2,4 +2,8 @@
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
 
     End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
 End Class
